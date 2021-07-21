45cc66d0bba03f6645ec1a0e6dc3e402f0d4ed62
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-21 14:46:12 -0400

    fixed destructuring

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,18 +1,18 @@
-function Article({postsObj: {title, date="January 1, 1970", preview, id, minutes}}){
+function Article( { title, date = "January 1, 1970", preview, id, minutes } ) {
     let timeToRead = minutes < 30 ? 5 : 10;
     let emoji = minutes < 30 ? "☕️" : "🍱";
     let emojis = ""
-    for (let i=0; minutes>i; i+= timeToRead) {
-        emojis += emoji 
-    }   
-    
-    return(
+    for (let i = 0; minutes > i; i += timeToRead) {
+        emojis += emoji
+    }
+
+    return (
         <article>
             <h3>{title}</h3>
             <small>{date}</small> · <small>{emojis}{minutes} min read</small>
             <p>{preview}</p>
         </article>
-    ) 
+    )
 }
 // do not need key={id} here - that is in ArticleList
 
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ function ArticleList(props) {
     console.log(props)
     return (
         <main>
-            {props.posts.map((post) => <Article key={post.id} postsObj={post}/>)}
+            {props.posts.map((post) => <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>)}
         </main>
     )
 }    
